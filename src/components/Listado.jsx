1b8278ad26bf6c4d9986d919f5b0873481e5a94b
Krismar Faria <krismarfaria27@gmail.com>
2022-03-12 21:00:34 +0100

    Formulario Terminado

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,29 +1,38 @@
-import { Paciente } from "./Paciente";
+import Paciente from "./Paciente"
 
-export const Listado = ({pacientes}) => {
+const Listado = ({pacientes, setPaciente, eliminarPaciente}) => {
     return (
-        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll pt-10">
-     <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
-     <p className="text-xl mt-5 text-center mb-10">
-                Administra tus {''}
-                <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-            </p>
-          
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
 
-{ pacientes.map( ( paciente, index ) => (
-          <Paciente
-          key={index}
-          paciente={paciente}
-          
-          />
-           ) ) }
+            {pacientes && pacientes.length ? (
+                <>
+                    <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Administra tus {''}
+                        <span className="text-indigo-600 font-bold ">Pacientes y Citas</span>
+                    </p>
 
+                    { pacientes.map( paciente => (
+                        <Paciente 
+                            key={paciente.id}
+                            paciente={paciente}
+                            setPaciente={setPaciente}
+                            eliminarPaciente={eliminarPaciente}
+                        />
+                    ))}
+                </>
 
-
-
-
-     </div>
+            ) : (
+                <>
+                    <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+                    <p className="text-xl mt-5 mb-10 text-center">
+                        Comienza agregando pacientes {''}
+                        <span className="text-indigo-600 font-bold ">y aparecerán en este lugar</span>
+                    </p>
+                </>
+            )}
+        </div>
     )
 }
 
-export default Listado;
+export default Listado
